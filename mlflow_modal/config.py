4255e4b124ca9d24891646b32fa8ff90f6eec697
garretthoffman <garrett.lee.hoffman@gmail.com>
2023-02-08 22:30:10 -0500

    update config to validate environment

--- mlflow_modal/config.py
+++ mlflow_modal/config.py
@@ -1,8 +1,23 @@
 import os
-import typing
+
+from mlflow.exceptions import MissingConfigException
+
+EXPECTED_ENV_VARs = ["MODAL_TOKEN_ID", "MODAL_TOKEN_SECRET", "MODAL_WORKSPACE"]
 
 
-class ModalConfig(typing.NamedTuple):
-    token_id: str = os.environ.get("MODAL_TOKEN_ID")
-    token_secret: str = os.environ.get("MODAL_TOKEN_SECRET")
-    workspace: str = os.environ.get("MODAL_WORKSPACE")
+class ModalConfig:
+    """
+    A class to validate and parse required mlflow-modal configuration
+    """
+
+    def __init__(self) -> None:
+        self._validate_enironment()
+
+        self.workspace: str = os.environ.get("MODAL_WORKSPACE")
+
+    def _validate_enironment(self) -> None:
+        for env_var in EXPECTED_ENV_VARs:
+            if env_var not in os.environ:
+                raise MissingConfigException(
+                    f"mlflow-modal requires {env_var} to be set."
+                )
